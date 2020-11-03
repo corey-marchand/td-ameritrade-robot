56cd9bf435f790c6c4aaa027b1d512ef82f479ca
corey-marchand1107 <coreymarchand.development@gmail.com>
2020-11-02 19:43:06 -0800

    setting up

---
+++ pyrobot/robot.py
@@ -0,0 +1,34 @@
+import pandas as pdb
+
+from td.client import TDClient
+from td.utils import milliseconds_since_epoch
+
+from datetime import datetime
+from datetime import time
+from datetime import timezone
+
+from typing import List
+from typing import Dict
+from typing import Union
+
+class PyRobot():
+    def __init__(self, client_id: str, redirect_uri: str, credentials_path: str = None, trading_account: str = None) -> None:
+        self.trading_account: str = trading_account
+        self.client_id : str = client_id
+        self.redirect_uri: str = redirect_uri
+        self.credentials_path: str = credentials_path
+        self.session: TDClient = self.create_session()
+        self.trades: dict = {}
+        self.historical_prices: dict = {}
+        self.stock_frame = None 
+
+    def create_session(self) -> TDClient:
+        td_client = TDClient(
+            client_id=self.client_id,
+            redirect_uri=self.redirect_uri,
+            credentials_path=self.credentials_path
+        )
+
+        td_client.login()
+
+        return td_client
